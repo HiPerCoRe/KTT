0d50770d8a55613bb09e997888c990c52b9246fd
Jiri Filipovic <fila@mail.muni.cz>
2018-04-27 21:09:06 +0200

    - some refactoring and bugfixing - performs multiple tunings

--- examples/gemm_batch/demo.cpp
+++ examples/gemm_batch/demo.cpp
@@ -116,6 +116,46 @@ private:
     int batch, a, b, c;
 };
 
+void tuneKernel(ktt::Tuner* tuner, std::string& kernelFile, ktt::ArgumentId& aID, ktt::ArgumentId& bID, ktt::ArgumentId &dstID, ktt::ArgumentId& nID, int a, int b, int c, int batch) {
+    // create kernel
+    ktt::DimensionVector ndRangeDimensions(batch);
+    ktt::DimensionVector workGroupDimensions;
+    ktt::KernelId kernelId = tuner->addKernelFromFile(kernelFile, "gemm_batch", ndRangeDimensions, workGroupDimensions);
+
+    // assign arguments
+    tuner->setKernelArguments(kernelId, std::vector<ktt::ArgumentId>{aID, bID, dstID, nID});
+
+    // create tuning space
+    tuner->addParameter(kernelId, "SIZE_A", {(size_t)a});
+    tuner->addParameter(kernelId, "SIZE_B", {(size_t)b});
+    tuner->addParameter(kernelId, "SIZE_C", {(size_t)c});
+    tuner->addParameter(kernelId, "GRANULARITY", {/*1, */2, 3}); // 1 = fine (matrix per thread), 2 = medium (block of a), 3 = coarse (block of a*b)
+    tuner->addParameter(kernelId, "GROUP_SIZE_X", {1, 32, 64, 128, 256, 512});
+    tuner->addParameter(kernelId, "MGCG_GROUP_SIZE_X", {1, (size_t)c});
+    tuner->addParameter(kernelId, "MGCG_GROUP_SIZE_Y", {1, 2, 4, 8, 16, 32});
+    tuner->addParameter(kernelId, "CACHING_STRATEGY", {0, 1, 2}); /* 0 = implicit caching, 1 = local memory, 2 = private memory */
+    auto parallelismConstraint = [](const std::vector<size_t>& v) {return (v[0] == 1 && v[1] > 1 && v[2] == 1 && v[3] == 1) || (v[0] == 2 && v[1] == 1 && v[2] > 1) || (v[0] == 3 && v[1] == 1 && v[2] > 1);};
+    tuner->addConstraint(kernelId, parallelismConstraint, {"GRANULARITY", "GROUP_SIZE_X", "MGCG_GROUP_SIZE_X", "MGCG_GROUP_SIZE_Y"});
+    auto tmpConstraint = [](const std::vector<size_t>& v) {return (v[0] < 3 || v[1] < 2);};
+    tuner->addConstraint(kernelId, tmpConstraint, {"GRANULARITY", "CACHING_STRATEGY"});
+    auto smConstraint = [](const std::vector<size_t>& v) {return (v[0] != 2) || (v[1]*v[2] + v[3]*v[1] + v[3]*v[2])*v[4]*sizeof(REAL) < 48*1024;};
+    tuner->addConstraint(kernelId, smConstraint, {"GRANULARITY", "SIZE_A", "SIZE_B", "SIZE_C", "MGCG_GROUP_SIZE_Y"});
+
+    // assign manipulator
+    tuner->setTuningManipulator(kernelId, std::make_unique<cTunableGemm>(batch, a, b, c));
+
+    // tune kernel   
+    std::vector<REAL> firstMatrix(32*32);
+    ktt::OutputDescriptor output(dstID, (void*)firstMatrix.data(), 32*32*sizeof(REAL));
+    for (int i = 0; i < STEPS; i++)
+        tuner->tuneKernelByStep(kernelId, {output});
+
+    // print best
+    std::pair<std::vector<ktt::ParameterPair>, double> bestConf = tuner->getBestConfiguration(kernelId);
+    std::cout << "Performance: " << (double)(a*b*c*2)*(double)batch / std::get<1>(bestConf) << " GFlops" << std::endl;
+    std::cout << "Memory BW: " << (double)(a*b+c*a+c*b)*(double)(batch)*(double)sizeof(REAL) / std::get<1>(bestConf) << " GB/s" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     // Initialize platform and device index
@@ -138,19 +178,6 @@ int main(int argc, char** argv)
     unsigned int c = 32;
     int batch = 1024*64;
 
-    /*if (argc >= 7) {
-        a = std::stoul(std::string(argv[3]));
-        b = std::stoul(std::string(argv[4]));
-        c = std::stoul(std::string(argv[5]));
-        batch = std::stoul(std::string(argv[6]));
-    }*/
-
-    /*std::cout << "Computing C = AB using " << batch << " matrices of sizes"
-        << std::endl
-        << "A: " << a << " x " << b << std::endl
-        << "B: " << c << " x " << a << std::endl
-        << "C: " << c << " x " << b << std::endl;*/
-
     std::vector<REAL> srcA(a*b*batch, 0.0f);
     std::vector<REAL> srcB(c*a*batch, 0.0f);
     std::vector<REAL> dst(c*b*batch, 0.0f);
@@ -170,7 +197,7 @@ int main(int argc, char** argv)
     std::cin >> interface;
 
     ktt::Tuner* tuner = NULL; 
-    if (interface != 1) {
+    if (interface == 1) {
         tuner = new ktt::Tuner(platformIndex, deviceIndex);
         kernelFile = KTT_CL_KERNEL_FILE;
     }
@@ -180,18 +207,22 @@ int main(int argc, char** argv)
     }
     tuner->setGlobalSizeType(ktt::GlobalSizeType::CUDA);
 
-    // create kernel
-    ktt::DimensionVector ndRangeDimensions(batch);
-    ktt::DimensionVector workGroupDimensions;
-    ktt::KernelId kernelId = tuner->addKernelFromFile(kernelFile, "gemm_batch", ndRangeDimensions, workGroupDimensions);
-
     // create input/output
     ktt::ArgumentId srcAId = tuner->addArgumentVector(srcA, ktt::ArgumentAccessType::ReadOnly);
     ktt::ArgumentId srcBId = tuner->addArgumentVector(srcB, ktt::ArgumentAccessType::ReadOnly);
     ktt::ArgumentId dstId = tuner->addArgumentVector(dst, ktt::ArgumentAccessType::WriteOnly);
     ktt::ArgumentId nId = tuner->addArgumentScalar(batch);
+
+#if 0
+    // create kernel
+    ktt::DimensionVector ndRangeDimensions(batch);
+    ktt::DimensionVector workGroupDimensions;
+    ktt::KernelId kernelId = tuner->addKernelFromFile(kernelFile, "gemm_batch", ndRangeDimensions, workGroupDimensions);
+
+    // assign arguments
     tuner->setKernelArguments(kernelId, std::vector<ktt::ArgumentId>{srcAId, srcBId, dstId, nId});
 
+    // create tuning space
     tuner->addParameter(kernelId, "SIZE_A", {(size_t)a});
     tuner->addParameter(kernelId, "SIZE_B", {(size_t)b});
     tuner->addParameter(kernelId, "SIZE_C", {(size_t)c});
@@ -200,7 +231,6 @@ int main(int argc, char** argv)
     tuner->addParameter(kernelId, "MGCG_GROUP_SIZE_X", {1, (size_t)c});
     tuner->addParameter(kernelId, "MGCG_GROUP_SIZE_Y", {1, 2, 4, 8, 16, 32});
     tuner->addParameter(kernelId, "CACHING_STRATEGY", {0, 1, 2}); /* 0 = implicit caching, 1 = local memory, 2 = private memory */
-
     auto parallelismConstraint = [](const std::vector<size_t>& v) {return (v[0] == 1 && v[1] > 1 && v[2] == 1 && v[3] == 1) || (v[0] == 2 && v[1] == 1 && v[2] > 1) || (v[0] == 3 && v[1] == 1 && v[2] > 1);};
     tuner->addConstraint(kernelId, parallelismConstraint, {"GRANULARITY", "GROUP_SIZE_X", "MGCG_GROUP_SIZE_X", "MGCG_GROUP_SIZE_Y"});
     auto tmpConstraint = [](const std::vector<size_t>& v) {return (v[0] < 3 || v[1] < 2);};
@@ -208,24 +238,26 @@ int main(int argc, char** argv)
     auto smConstraint = [](const std::vector<size_t>& v) {return (v[0] != 2) || (v[1]*v[2] + v[3]*v[1] + v[3]*v[2])*v[4]*sizeof(REAL) < 48*1024;};
     tuner->addConstraint(kernelId, smConstraint, {"GRANULARITY", "SIZE_A", "SIZE_B", "SIZE_C", "MGCG_GROUP_SIZE_Y"});
 
-    //tuner->setReferenceClass(kernelId, std::make_unique<referenceGemm>(srcA, srcB, a, b, c, batch, dstId), std::vector<ktt::ArgumentId>{dstId});
-    //tuner->setValidationMethod(ktt::ValidationMethod::SideBySideComparison, 0.001f);
-    //tuner->setValidationRange(dstId, c*b*batch);
+    /*tuner->setReferenceClass(kernelId, std::make_unique<referenceGemm>(srcA, srcB, a, b, c, batch, dstId), std::vector<ktt::ArgumentId>{dstId});
+    tuner->setValidationMethod(ktt::ValidationMethod::SideBySideComparison, 0.001f);
+    tuner->setValidationRange(dstId, c*b*batch);*/
 
     tuner->setTuningManipulator(kernelId, std::make_unique<cTunableGemm>(batch, a, b, c));
-   
+
+    // tune kernel   
     std::vector<REAL> firstMatrix(32*32);
     ktt::OutputDescriptor output(dstId, (void*)firstMatrix.data(), 32*32*sizeof(REAL));
     for (int i = 0; i < STEPS; i++)
         tuner->tuneKernelByStep(kernelId, {output});
-    //tuner->tuneKernel(kernelId);
-    //tuner->printResult(kernelId, std::cout, ktt::PrintFormat::Verbose);
-    //tuner->printResult(kernelId, "gemm_batch_output.csv", ktt::PrintFormat::CSV);
 
+    // print best
     std::pair<std::vector<ktt::ParameterPair>, double> bestConf = tuner->getBestConfiguration(kernelId);
-
     std::cout << "Performance: " << (double)(a*b*c*2)*(double)batch / std::get<1>(bestConf) << " GFlops" << std::endl;
     std::cout << "Memory BW: " << (double)(a*b+c*a+c*b)*(double)(batch)*(double)sizeof(REAL) / std::get<1>(bestConf) << " GB/s" << std::endl;
+#endif
+
+    for (int i = 0; i < 10; i++)
+        tuneKernel(tuner, kernelFile, srcAId, srcBId, dstId, nId, a, b, c, batch);
 
     delete tuner;
 
