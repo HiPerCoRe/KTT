6f9e48b244b46e4ec5ac296051663bde2087c99b
Jiří Filipovič <72898@mail.muni.cz>
2018-11-22 14:15:24 +0100

    removed useless optimizations

--- examples/gemm_batch/gemm_kernel.cu
+++ examples/gemm_batch/gemm_kernel.cu
@@ -125,7 +125,7 @@ extern "C" __global__ void gemm_batch(const REAL* A, const REAL* B, REAL* C, int
 }
 #endif
 
-#if GRANULARITY == 3 and RTILE == 1
+#if GRANULARITY == 3
 extern "C" __global__ void gemm_batch(const REAL* A, const REAL* B, REAL* C, int n) {
     int matrix = blockIdx.x;
     int tx = threadIdx.x;
@@ -169,102 +169,3 @@ extern "C" __global__ void gemm_batch(const REAL* A, const REAL* B, REAL* C, int
 }
 #endif
 
-#if (GRANULARITY == 3 && RTILE > 1)
-
-const int SIZE_A_UP = (SIZE_A/RTILE + (SIZE_A%RTILE == 0 ? 0 : 1))*RTILE;
-const int SIZE_A_BLOCKS = SIZE_A_UP / RTILE;
-
-extern "C" __global__ void gemm_batch(const REAL* A, const REAL* B, REAL* C, int n) {
-    int matrix = blockIdx.x;
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-
-/* preload data */
-#if CACHING_STRATEGY == 1
-    int preloadStartA = blockIdx.x*SIZE_A*SIZE_B;
-    int preloadStartB = blockIdx.x*SIZE_C*SIZE_A;
-    __shared__ REAL bufA[SIZE_B*SIZE_A];
-    __shared__ REAL bufB[SIZE_A*SIZE_C];
-    for (int i = ty*blockDim.x+tx; i < SIZE_A*SIZE_B; i+= blockDim.x*MGCG_GROUP_SIZE_Y) // MGCG_GROUP_SIZE_Y = SIZE_B/RTILE
-        bufA[i] = A[preloadStartA + i];
-    for (int i = ty*blockDim.x+tx; i < SIZE_C*SIZE_A; i+= blockDim.x*MGCG_GROUP_SIZE_Y)
-        bufB[i] = B[preloadStartB + i];
-    __syncthreads(); //XXX
-    /*if (tx == 0 && ty == 0)
-        for (int i = 0; i < SIZE_A; i++) {
-            for (int j = 0; j < SIZE_C; j++)
-                printf("%f ", bufB[i][j]);
-            printf("\n");
-        }*/
-    /*if (tx == 0 && ty == 0)
-        for (int i = 0; i < SIZE_B; i++) {
-            for (int j = 0; j < SIZE_A; j++)
-                printf("%f ", bufA[i*SIZE_A+j]);
-            printf("\n");
-        }*/
-#endif
-/* offsets into memory */
-#if CACHING_STRATEGY == 0
-    int startA = matrix*SIZE_A*SIZE_B;
-    int startB = matrix*SIZE_C*SIZE_A;
-#else
-    int startA = 0;
-    int startB = 0;
-#endif
-    int startC = matrix*SIZE_C*SIZE_B;
-
-/* declare block in registers */
-    REAL rbufA[RTILE][RTILE];
-    REAL rbufB[RTILE][RTILE];
-    REAL tmp[RTILE][RTILE];
-    for (int i = 0; i < RTILE; i++)
-        for (int j = 0; j < RTILE; j++)
-            tmp[i][j] = (REAL)0.0;
-
-/* iterate over shared memory */
-    for (int block = 0; block < SIZE_A_BLOCKS; block++) {
-        for (int i = 0; i < RTILE; i++)
-            for (int j = 0; j < RTILE; j++) {
-                rbufA[i][j] = bufA[(ty*RTILE+i)*SIZE_A + block*RTILE + j];
-                rbufB[i][j] = bufB[(block*RTILE+i)*SIZE_C + tx*RTILE+j];
-        }
-#if RTILE == 4
-        /*if (tx == 0 && ty == 1) {
-            for (int i = 0; i < RTILE; i++) 
-                //printf("%f %f %f %f\n", rbufA[i][0], rbufA[i][1], rbufA[i][2], rbufA[i][3]);
-                printf("%f %f %f %f\n", rbufB[i][0], rbufB[i][1], rbufB[i][2], rbufB[i][3]);
-            printf("\n");
-        }*/
-#endif
-    /* compute multiplication */
-        for (int i = 0; i < RTILE; i++)
-            for (int j = 0; j < RTILE; j++)
-                for (int k = 0; k < RTILE; k++)
-                    tmp[j][i] += rbufA[j][k]*rbufB[k][i];
-/*      for (int i = 0; i < RTILE; i++) 
-            printf("%f %f %f %f\n", tmp[i][0], tmp[i][1], tmp[i][2], tmp[i][3]);*/
-    }
-/* store results*/
-#if RTILE == 2
-    REAL2 storeVec;
-    for (int i = 0; i < RTILE; i++) {
-        storeVec.x = tmp[i][0];
-        storeVec.y = tmp[i][1];
-        REAL2 *out = reinterpret_cast<REAL2*>(C);
-        out[(startC + (i+ty*RTILE)*SIZE_C + tx*RTILE)/2] = storeVec;
-    }
-#endif
-#if RTILE == 4
-    REAL4 storeVec;
-    for (int i = 0; i < RTILE; i++) {
-        storeVec.x = tmp[i][0];
-        storeVec.y = tmp[i][1];
-        storeVec.z = tmp[i][2];
-        storeVec.w = tmp[i][3];
-//      printf("%f %f %f %f\n", storeVec.x, storeVec.y, storeVec.z, storeVec.w);
-        REAL4 *out = reinterpret_cast<REAL4*>(C);
-        out[(startC + (i+ty*RTILE)*SIZE_C + tx*RTILE)/4] = storeVec;
-    }
-#endif
-}
-#endif
