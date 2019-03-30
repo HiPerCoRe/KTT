95933dcc1e8835eaf998b6a3adc5fe2b2a6d4042
Fillo7 <mail.filip.petrovic@gmail.com>
2019-03-30 11:01:43 +0100

    Fixed compilation error with certain versions of Vulkan SDK

--- libraries/include/shaderc_ktt/shaderc.h
+++ libraries/include/shaderc_ktt/shaderc.h
@@ -23,9 +23,9 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
-#include "shaderc/env.h"
-#include "shaderc/status.h"
-#include "shaderc/visibility.h"
+#include "env.h"
+#include "status.h"
+#include "visibility.h"
 
 // Source language kind.
 typedef enum {
