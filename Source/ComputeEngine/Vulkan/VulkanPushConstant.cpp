829086d0e2506ec15e884d57a25eb050ef32c5c8
Fillo7 <filip.petrovic7@protonmail.com>
2021-05-13 11:13:18 +0200

    * Implemented Vulkan specialization constants and push constants

---
+++ Source/ComputeEngine/Vulkan/VulkanPushConstant.cpp
@@ -0,0 +1,55 @@
+#ifdef KTT_API_VULKAN
+
+#include <ComputeEngine/Vulkan/VulkanPushConstant.h>
+#include <Utility/ErrorHandling/Assert.h>
+
+namespace ktt
+{
+
+VulkanPushConstant::VulkanPushConstant(const std::vector<KernelArgument*>& arguments)
+{
+    m_Range.size = 0;
+    m_Range.offset = 0;
+    m_Range.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+
+    for (const auto* argument : arguments)
+    {
+        KttAssert(argument->GetMemoryType() == ArgumentMemoryType::Scalar, "Only scalar arguments can be used as push constants");
+        const size_t dataSize = argument->GetDataSize();
+
+        for (size_t i = 0; i < dataSize; ++i)
+        {
+            m_Data.push_back(argument->GetDataWithType<uint8_t>()[i]);
+        }
+
+        m_Range.size += static_cast<uint32_t>(dataSize);
+    }
+
+    uint32_t paddingSize = 0;
+
+    if (m_Range.size % 4 != 0)
+    {
+        paddingSize = 4 - m_Range.size % 4;
+    }
+
+    for (uint32_t i = 0; i < paddingSize; ++i)
+    {
+        m_Data.push_back(0);
+    }
+
+    m_Range.size += paddingSize;
+}
+
+const VkPushConstantRange& VulkanPushConstant::GetRange() const
+{
+    return m_Range;
+}
+
+const std::vector<uint8_t>& VulkanPushConstant::GetData() const
+{
+    return m_Data;
+}
+
+} // namespace ktt
+
+#endif // KTT_API_VULKAN
---
+++ Source/ComputeEngine/Vulkan/VulkanSpecializationConstant.cpp
@@ -0,0 +1,44 @@
+#ifdef KTT_API_VULKAN
+
+#include <ComputeEngine/Vulkan/VulkanSpecializationConstant.h>
+#include <Utility/ErrorHandling/Assert.h>
+
+namespace ktt
+{
+
+VulkanSpecializationConstant::VulkanSpecializationConstant(const std::vector<KernelArgument*>& arguments)
+{
+    for (size_t i = 0; i < arguments.size(); ++i)
+    {
+        const auto& argument = *arguments[i];
+        KttAssert(argument.GetMemoryType() == ArgumentMemoryType::Scalar, "Only scalar arguments can be used as specialization constants");
+
+        VkSpecializationMapEntry entry =
+        {
+            static_cast<uint32_t>(i),
+            static_cast<uint32_t>(m_Data.size()),
+            argument.GetDataSize()
+        };
+
+        for (size_t j = 0; j < argument.GetDataSize(); ++j)
+        {
+            m_Data.push_back(argument.GetDataWithType<uint8_t>()[j]);
+        }
+
+        m_MapEntries.push_back(entry);
+    }
+
+    m_SpecializationInfo.mapEntryCount = static_cast<uint32_t>(m_MapEntries.size()),
+    m_SpecializationInfo.pMapEntries = m_MapEntries.data();
+    m_SpecializationInfo.dataSize = m_Data.size();
+    m_SpecializationInfo.pData = m_Data.data();
+}
+
+const VkSpecializationInfo& VulkanSpecializationConstant::GetSpecializationInfo() const
+{
+    return m_SpecializationInfo;
+}
+
+} // namespace ktt
+
+#endif // KTT_API_VULKAN
