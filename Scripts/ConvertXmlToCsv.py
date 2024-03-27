597c5f6cdbc0ef657ac065c9b7868a670762a5a2
Jana Hozzova <hozzova@ics.muni.cz>
2024-03-27 12:23:02 +0100

    Remove whitespaces when converting output to csv

--- Scripts/ConvertXmlToCsv.py
+++ Scripts/ConvertXmlToCsv.py
@@ -11,27 +11,27 @@ myroot = mytree.getroot()
 res = myroot.find('Results')
 
 # Create CSV header
-print('Kernel name, Computation duration (us), Global size, Local size', end = ', ')
+print('Kernel name,Computation duration (us),Global size,Local size', end = ',')
 for kr in res.findall('KernelResult') :
     if kr.attrib['Status'] == 'Ok' :
         conf = kr.find('Configuration')
         for tp in conf.findall('Pair') :
-            print(tp.attrib['Name'], end = ', ')
+            print(tp.attrib['Name'], end = ',')
         crs = kr.find('ComputationResults')
         cr = crs.find('ComputationResult')
         pd = cr.find('ProfilingData')
         if pd != None :
             cts = pd.find('Counters')
             for ct in cts.findall('Counter') :
-                print(ct.attrib['Name'], end = ', ')
+                print(ct.attrib['Name'], end = ',')
         break
-print('Maximum work-group size, Local memory size, Private memory size, Constant memory size, Registers count')
+print('Maximum work-group size,Local memory size,Private memory size,Constant memory size,Registers count')
 
 # Extract data into CSV
 for kr in res.findall('KernelResult') :
     if kr.attrib['Status'] == 'Ok' :
-        print(kr.attrib['KernelName'], end = ', ')
-        print(kr.attrib['TotalDuration'], end = ', ')
+        print(kr.attrib['KernelName'], end = ',')
+        print(kr.attrib['TotalDuration'], end = ',')
 
         crs = kr.find('ComputationResults')
         cr = crs.find('ComputationResult')
@@ -39,18 +39,18 @@ for kr in res.findall('KernelResult') :
         ls = cr.find('LocalSize')
         gsi = int(gs.attrib['X']) * int(gs.attrib['Y']) * int(gs.attrib['Z'])
         lsi = int(ls.attrib['X']) * int(ls.attrib['Y']) * int(ls.attrib['Z'])
-        print(gsi*lsi, end = ', ') #TODO manage OpenCL/CUDA format!
-        print(lsi, end = ', ')
+        print(gsi*lsi, end = ',') #TODO manage OpenCL/CUDA format!
+        print(lsi, end = ',')
 
         conf = kr.find('Configuration')
         for tp in conf.findall('Pair') :
-            print(tp.attrib['Value'], end = ', ')
+            print(tp.attrib['Value'], end = ',')
 
         pd = cr.find('ProfilingData')
         if pd != None :
             cts = pd.find('Counters')
             for ct in cts.findall('Counter') :
-                print(ct.attrib['Value'], end = ', ')
+                print(ct.attrib['Value'], end = ',')
 
         cd = cr.find('CompilationData')
-        print(cd.attrib['MaxWorkGroupSize'] + ', ' + cd.attrib['LocalMemorySize'] + ', ' + cd.attrib['PrivateMemorySize'] + ', ' + cd.attrib['ConstantMemorySize'] + ', ' + cd.attrib['RegistersCount'])
+        print(cd.attrib['MaxWorkGroupSize'] + ',' + cd.attrib['LocalMemorySize'] + ',' + cd.attrib['PrivateMemorySize'] + ',' + cd.attrib['ConstantMemorySize'] + ',' + cd.attrib['RegistersCount'])
