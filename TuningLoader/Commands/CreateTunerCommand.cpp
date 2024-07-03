be5831428fa7b1e7171edeacc0ef60a5191a694e
Jana Hozzova <hozzova@ics.muni.cz>
2024-07-03 08:50:08 +0200

    Add Device into input json schema for TuningLoader

--- TuningLoader/Commands/CreateTunerCommand.cpp
+++ TuningLoader/Commands/CreateTunerCommand.cpp
@@ -3,13 +3,15 @@
 namespace ktt
 {
 
-CreateTunerCommand::CreateTunerCommand(const ComputeApi api) :
+CreateTunerCommand::CreateTunerCommand(const PlatformIndex platformId, const DeviceIndex deviceId, const ComputeApi api) :
+    m_PlatformId(platformId),
+    m_DeviceId(deviceId),
     m_Api(api)
 {}
 
 void CreateTunerCommand::Execute(TunerContext& context)
 {
-    auto tuner = std::make_unique<Tuner>(0, 0, m_Api);
+    auto tuner = std::make_unique<Tuner>(m_PlatformId, m_DeviceId, m_Api);
     context.SetTuner(std::move(tuner));
 }
 
