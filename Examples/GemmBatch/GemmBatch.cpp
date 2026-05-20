b99f21539c9b5a22c463f421d716ad249401958b
Petr Slonek <48959829+Petronous@users.noreply.github.com>
2026-05-20 20:39:59 +0200

    Refactor GemmBatch

--- Examples/GemmBatch/GemmBatch.cpp
+++ Examples/GemmBatch/GemmBatch.cpp
@@ -1,258 +1,151 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Ktt.h"
+#include "../ExampleReferenceComputation.h"
 
-#if defined(_MSC_VER)
-    const std::string kernelFilePrefix = "";
-#else
-    const std::string kernelFilePrefix = "../";
-#endif
+using namespace std;
 
-#if KTT_CUDA_EXAMPLE
-    const std::string defaultKernelFile = kernelFilePrefix + "../examples/gemm_batch/gemm_kernel.cu";
-    const auto computeAPI = ktt::ComputeAPI::CUDA;
-#elif KTT_OPENCL_EXAMPLE
-    const std::string defaultKernelFile = kernelFilePrefix + "../examples/gemm_batch/gemm_kernel.cl";
-    const auto computeAPI = ktt::ComputeAPI::OpenCL;
-#endif
-
-#define RAPID_TEST 0
-#define USE_PROFILING 0
-
-#define REAL float
-
-class referenceGemm : public ktt::ReferenceClass
-{
-public:
-    referenceGemm(const std::vector<REAL>& srcA, const std::vector<REAL>& srcB, const int a, const int b, const int c, const int n, const ktt::ArgumentId resultArgumentId) :
-        srcA(srcA),
-	    srcB(srcB),
-    	a(a),
-    	b(b),
-	    c(c),
-    	n(n),
-        resultArgumentId(resultArgumentId)
-    {}
-
-    void computeResult() override {
-        res.resize(n*c*b);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < c; j++)
-                for (int k = 0; k < b; k++) {
-                    REAL tmp = 0.0f;
-                    for (int l = 0; l < a; l++)
-                        tmp += srcA[i*a*b + k*a + l] * srcB[i*c*a + l*c + j];
-                    res[i*c*b + k*c + j] = tmp;
-                }
-            /*if (i == 0) {
-                for (int bl = 0; bl < b; bl++){
-                    for (int al = 0; al < a; al++)
-                        std::cout << srcA[i*a*b + bl*a + al] << " ";
-                    std::cout << "\n";
-                }
-                for (int al = 0; al < a; al++){
-                    for (int cl = 0; cl < c; cl++)
-                        std::cout << srcB[i*c*b + al*c + cl] << " ";
-                    std::cout << "\n";
-                }
-                for (int bl = 0; bl < b; bl++){
-                    for (int cl = 0; cl < c; cl++)
-                        std::cout << res[i*c*b + bl*c + cl] << " ";
-                    std::cout << "\n";
-                }
-            }*/
-	    }
-    }
-
-    void* getData(const ktt::ArgumentId id) override {
-        if (id == resultArgumentId) {
-            return (void*)res.data();
-        }
-        return nullptr;
-    }
-
-    size_t getNumberOfElements(const ktt::ArgumentId argumentId) const override {
-        return n*c*b;
-    }
-
-private:
-    std::vector<REAL> res;
-    std::vector<REAL> srcA;
-    std::vector<REAL> srcB;
-    int a, b, c, n;
-    ktt::ArgumentId resultArgumentId;
-};
-
-class cTunableGemm : public ktt::TuningManipulator {
-public:
-    cTunableGemm(const int batch, const int a, const int b, const int c)  :
-        batch(batch),
-        a(a),
-        b(b),
-        c(c)
-    {}
-    void launchComputation(const ktt::KernelId kernelId) override {
-        ktt::DimensionVector globalSize(1, 1, 1);
-        ktt::DimensionVector localSize(1, 1, 1);
-        std::vector<ktt::ParameterPair> parameterValues = getCurrentConfiguration();
-        size_t padd_c = getParameterValue("PADD_C", parameterValues);
-        size_t y = getParameterValue("GROUP_SIZE_Y", parameterValues);
-        size_t z = getParameterValue("GROUP_SIZE_Z", parameterValues);
-
-        if (computeAPI == ktt::ComputeAPI::OpenCL)
-        {
-            globalSize.setSizeX(batch*(c + padd_c) / z);
-            globalSize.setSizeY(y);
-            globalSize.setSizeZ(z);
-        }
-        else
-        {
-            globalSize.setSizeX(batch / z);
-        }
-
-        localSize.setSizeX(c+padd_c);
-        localSize.setSizeY(y);
-        localSize.setSizeZ(z);
-
-#if USE_PROFILING == 1
-        runKernelWithProfiling(kernelId, globalSize, localSize);
-#else
-        runKernel(kernelId, globalSize, localSize);
-#endif
-    }
-private:
-    int batch, a, b, c;
-};
-
-int main(int argc, char** argv)
-{
-    // Initialize platform and device index
-    ktt::PlatformIndex platformIndex = 0;
-    ktt::DeviceIndex deviceIndex = 0;
-    std::string kernelFile = defaultKernelFile;
-
-    if (argc >= 2)
+class GemmBatch : public ExampleReferenceComputation {
+protected:
+    GemmBatch(std::shared_ptr<ExampleConfiguration> config, int defaultProblemSize,
+              std::string exampleFolderPath, std::string defaultKernelFileBaseName) :
+        ExampleReferenceComputation(config, defaultProblemSize, exampleFolderPath, defaultKernelFileBaseName)
     {
-        platformIndex = std::stoul(std::string(argv[1]));
-        if (argc >= 3)
-        {
-            deviceIndex = std::stoul(std::string(argv[2]));
-        }
+        m_batch = m_problemSize * 16 * 1024;
     }
+
+    friend ExampleBase;
 
     // Declare and initialize data (m, n > 1)
-    unsigned int a = 4;
-    unsigned int b = 4;
-    unsigned int c = 4;
-    int batch = 1024*64;
+    const unsigned a = 4;
+    const unsigned b = 4;
+    const unsigned c = 4;
+    int m_batch;
 
-    if (argc >= 7) {
-        a = std::stoul(std::string(argv[3]));
-        b = std::stoul(std::string(argv[4]));
-        c = std::stoul(std::string(argv[5]));
-        batch = std::stoul(std::string(argv[6]));
-    }
+    std::vector<float> m_srcA;
+    std::vector<float> m_srcB;
+    std::vector<float> m_dst;
 
-    std::cout << "Computing C = AB using " << batch << " matrices of sizes"
-        << std::endl
-        << "A: " << a << " x " << b << std::endl
-        << "B: " << c << " x " << a << std::endl
-        << "C: " << c << " x " << b << std::endl;
+    ktt::ArgumentId m_srcAId;
+    ktt::ArgumentId m_srcBId;
+    ktt::ArgumentId m_dstId;
+    ktt::ArgumentId m_nId;
 
-    std::vector<REAL> srcA(a*b*batch, 0.0f);
-    std::vector<REAL> srcB(c*a*batch, 0.0f);
-    std::vector<REAL> dst(c*b*batch, 0.0f);
-
-    // fill with random data
-    for (size_t i = 0; i < a*b*batch; i++)
+    void InitData() override
     {
-        srcA[i] = 10.0f*((REAL)rand()) / ((REAL) RAND_MAX);
+        std::cout << "Computing C = AB using " << m_batch << " matrices of sizes"
+            << std::endl
+            << "A: " << a << " x " << b << std::endl
+            << "B: " << c << " x " << a << std::endl
+            << "C: " << c << " x " << b << std::endl;
+
+        m_srcA.resize(a * b * m_batch);
+        m_srcB.resize(c * a * m_batch);
+        m_dst.resize(c * b * m_batch);
+
+        FillBuffers({&m_srcA, &m_srcB}, 0.f, 10.f);
     }
-    for (size_t i = 0; i < c*a*batch; i++)
+
+    void InitKernel() override 
     {
-        srcB[i] = 10.0f*((REAL)rand()) / ((REAL) RAND_MAX);
+        ktt::DimensionVector ndRangeDimensions(m_batch);
+        ktt::DimensionVector workGroupDimensions;
+
+        // create input/output
+        m_srcAId = m_tuner.AddArgumentVector(m_srcA, ktt::ArgumentAccessType::ReadOnly);
+        m_srcBId = m_tuner.AddArgumentVector(m_srcB, ktt::ArgumentAccessType::ReadOnly);
+        m_dstId = m_tuner.AddArgumentVector(m_dst, ktt::ArgumentAccessType::WriteOnly);
+        m_nId = m_tuner.AddArgumentScalar(m_batch);
+
+        InitKernelDefault("gemm_batch", "GemmBatch", ndRangeDimensions, {m_srcAId, m_srcBId, m_dstId, m_nId});
+
+        m_tuner.SetLauncher(m_kernel, [this](ktt::ComputeInterface& interface) {
+            const std::vector<ktt::ParameterPair>& pairs = interface.GetCurrentConfiguration().GetPairs();
+
+            size_t padd_c = ktt::ParameterPair::GetParameterValue<uint64_t>(pairs, "PADD_C");
+            size_t y = ktt::ParameterPair::GetParameterValue<uint64_t>(pairs, "GROUP_SIZE_Y");
+            size_t z = ktt::ParameterPair::GetParameterValue<uint64_t>(pairs, "GROUP_SIZE_Z");
+
+            ktt::DimensionVector myGlobalSize(1, 1, 1);
+            ktt::DimensionVector myLocalSize(1, 1, 1);
+
+            // if (m_computeApi == ktt::ComputeApi::OpenCL)
+            // {
+            //     myGlobalSize.SetSizeX(m_batch * (c + padd_c) / z);
+            //     myGlobalSize.SetSizeY(y);
+            //     myGlobalSize.SetSizeZ(z);
+            // }
+            // else
+            // {
+                myGlobalSize.SetSizeX(m_batch / z);
+            // }
+
+            myLocalSize.SetSizeX(c + padd_c);
+            myLocalSize.SetSizeY(y);
+            myLocalSize.SetSizeZ(z);
+
+            interface.RunKernel(m_definition, myGlobalSize, myLocalSize);
+        });
     }
-/*    // fill transposed B
-    for (int i = 0; i < batch; i++)
-        for (int j = 0; j < c; j++)
-            for (int k = 0; k < a; k++)
-            {
-                srcBT[i*a*c + j*a + k] = srcB[i*a*c + k*c + j];
-            }*/
 
-    ktt::Tuner tuner(platformIndex, deviceIndex, computeAPI);
-    tuner.setPrintingTimeUnit(ktt::TimeUnit::Microseconds);
+    void InitTuningSpace() override 
+    {
+        m_tuner.AddParameter(m_kernel, "SIZE_A", vector<uint64_t>{(size_t)a});
+        m_tuner.AddParameter(m_kernel, "SIZE_B", vector<uint64_t>{(size_t)b});
+        m_tuner.AddParameter(m_kernel, "SIZE_C", vector<uint64_t>{(size_t)c});
+        m_tuner.AddParameter(m_kernel, "GROUP_SIZE_Y", vector<uint64_t>{1, 2, 4, 8, 16, 32});
+        m_tuner.AddParameter(m_kernel, "GROUP_SIZE_Z", vector<uint64_t>{1, 2, 4, 8, 16, 32, 64});
+        m_tuner.AddParameter(m_kernel, "CACHING_STRATEGY", vector<uint64_t>{0, 1, 2}); /* 0 = implicit caching, 1 = local memory, 2 = private memory */
+        m_tuner.AddParameter(m_kernel, "PADD_AA", vector<uint64_t>{0, 1});
+        m_tuner.AddParameter(m_kernel, "PADD_AB", vector<uint64_t>{0, 1});
+        if (c % 4 == 0)
+            m_tuner.AddParameter(m_kernel, "PADD_C", vector<uint64_t>{0});
+        else
+            m_tuner.AddParameter(m_kernel, "PADD_C", vector<uint64_t>{0, c % 4});
+        m_tuner.AddParameter(m_kernel, "DIRECT_WRITE", vector<uint64_t>{0, 1});
+        m_tuner.AddParameter(m_kernel, "UNROLL_K", vector<uint64_t>{0, 1});
 
-    #if USE_PROFILING == 1
-    printf("Executing with profiling switched ON.\n");
-    tuner.setKernelProfiling(true);
-    #endif
+        auto parallelismConstraint = [](const std::vector<size_t>& v) {return v[0] <= v[1];};
+        m_tuner.AddConstraint(m_kernel, {"GROUP_SIZE_Y", "SIZE_B"}, parallelismConstraint);
+        auto paddConstraint = [](const std::vector<size_t>& v) {return (v[0] == 0 && v[1] == 0 && v[2] == 0) || (v[3] > 0);};
+        m_tuner.AddConstraint(m_kernel, {"PADD_AA", "PADD_AB", "PADD_C", "CACHING_STRATEGY"}, paddConstraint);
+        auto dwConstraint = [](const std::vector<size_t>& v) {return (v[0] == 1) || (v[1] > 0);};
+        m_tuner.AddConstraint(m_kernel, {"DIRECT_WRITE", "CACHING_STRATEGY"}, dwConstraint);
+        auto unrollkConstraint = [](const std::vector<size_t>& v) {return (v[0] == 0) || (v[1] == 2);};
+        m_tuner.AddConstraint(m_kernel, {"UNROLL_K", "CACHING_STRATEGY"}, unrollkConstraint);
+    #define SHARED_PER_BLOCK (49152/4)
+        auto memConstraint = [](const std::vector<size_t>& v) {size_t a = v[1]; size_t b = v[2]; size_t c = v[3]; return (v[0] == 1 && ((a+v[7])*(b+v[8])+c*a+(1-v[4])*(c*b))*v[6] < SHARED_PER_BLOCK) || (v[0] == 2 && v[5] == 1 && ((a+v[7])*(b+v[8])+(1-v[4])*(c*b))*v[6] < SHARED_PER_BLOCK) || (v[0] == 2 && ((a+v[7])*(b+v[8])+c*a+(1-v[4])*(c*b))*v[6] < SHARED_PER_BLOCK);};
+        m_tuner.AddConstraint(m_kernel, {"CACHING_STRATEGY", "SIZE_A", "SIZE_B", "SIZE_C", "DIRECT_WRITE", "GROUP_SIZE_Y", "GROUP_SIZE_Z", "PADD_AA", "PADD_AB"}, memConstraint);
+    #define MAX_BLOCK_SIZE 1024
+        auto blockConstraint = [](const std::vector<size_t>&v) {return ((v[0]+v[2])*v[1]*v[3] < MAX_BLOCK_SIZE) && ((v[0]+v[2])*v[1]*v[3] >= 32);};
+        m_tuner.AddConstraint(m_kernel, {"SIZE_C", "GROUP_SIZE_Y", "PADD_C", "GROUP_SIZE_Z"}, blockConstraint);
+    }
 
-    // create kernel
-    ktt::DimensionVector ndRangeDimensions(batch);
-    ktt::DimensionVector workGroupDimensions;
-    ktt::KernelId kernelId = tuner.addKernelFromFile(kernelFile, "gemm_batch", ndRangeDimensions, workGroupDimensions);
+    void InitReference() override
+    {
+        m_tuner.SetReferenceComputation(m_dstId, [this](void* buffer) {
+            std::vector<float> res(m_batch * c * b);
 
-    // create input/output
-    ktt::ArgumentId srcAId = tuner.addArgumentVector(srcA, ktt::ArgumentAccessType::ReadOnly);
-    ktt::ArgumentId srcBId = tuner.addArgumentVector(srcB, ktt::ArgumentAccessType::ReadOnly);
-    ktt::ArgumentId dstId = tuner.addArgumentVector(dst, ktt::ArgumentAccessType::WriteOnly);
-    ktt::ArgumentId nId = tuner.addArgumentScalar(batch);
-    tuner.setKernelArguments(kernelId, std::vector<ktt::ArgumentId>{srcAId, srcBId, dstId, nId});
+            for (int i = 0; i < m_batch; i++) {
+                for (unsigned j = 0; j < c; j++) {
+                    for (unsigned k = 0; k < b; k++) {
+                        float tmp = 0.0f;
+                        for (unsigned l = 0; l < a; l++) {
+                            tmp += m_srcA[i*a*b + k*a + l] * m_srcB[i*c*a + l*c + j];
+                        }
+                        res[i*c*b + k*c + j] = tmp;
+                    }
+                }
+            }
 
-#if RAPID_TEST == 1
-    tuner.persistArgument(srcAId, true);
-    tuner.persistArgument(srcBId, true);
-    tuner.persistArgument(dstId, true);
-#endif
+            std::memcpy(buffer, res.data(), res.size() * sizeof(float));
+        });
+    }
+};
 
-    tuner.addParameter(kernelId, "SIZE_A", {(size_t)a});
-    tuner.addParameter(kernelId, "SIZE_B", {(size_t)b});
-    tuner.addParameter(kernelId, "SIZE_C", {(size_t)c});
-    tuner.addParameter(kernelId, "GROUP_SIZE_Y", {1, 2, 4, 8, 16, 32});
-    tuner.addParameter(kernelId, "GROUP_SIZE_Z", {1, 2, 4, 8, 16, 32, 64});
-    tuner.addParameter(kernelId, "CACHING_STRATEGY", {0, 1, 2}); /* 0 = implicit caching, 1 = local memory, 2 = private memory */
-    tuner.addParameter(kernelId, "PADD_AA", {0, 1});
-    tuner.addParameter(kernelId, "PADD_AB", {0, 1});
-    if (c % 4 == 0)
-        tuner.addParameter(kernelId, "PADD_C", {0});
-    else
-        tuner.addParameter(kernelId, "PADD_C", {0, c % 4});
-    tuner.addParameter(kernelId, "DIRECT_WRITE", {0, 1});
-    tuner.addParameter(kernelId, "UNROLL_K", {0, 1});
 
-    auto parallelismConstraint = [](const std::vector<size_t>& v) {return v[0] <= v[1];};
-    tuner.addConstraint(kernelId, {"GROUP_SIZE_Y", "SIZE_B"}, parallelismConstraint);
-    auto paddConstraint = [](const std::vector<size_t>& v) {return (v[0] == 0 && v[1] == 0 && v[2] == 0) || (v[3] > 0);};
-    tuner.addConstraint(kernelId, {"PADD_AA", "PADD_AB", "PADD_C", "CACHING_STRATEGY"}, paddConstraint);
-    auto dwConstraint = [](const std::vector<size_t>& v) {return (v[0] == 1) || (v[1] > 0);};
-    tuner.addConstraint(kernelId, {"DIRECT_WRITE", "CACHING_STRATEGY"}, dwConstraint);
-    auto unrollkConstraint = [](const std::vector<size_t>& v) {return (v[0] == 0) || (v[1] == 2);};
-    tuner.addConstraint(kernelId, {"UNROLL_K", "CACHING_STRATEGY"}, unrollkConstraint);
-#define SHARED_PER_BLOCK (49152/4)
-    auto memConstraint = [](const std::vector<size_t>& v) {size_t a = v[1]; size_t b = v[2]; size_t c = v[3]; return (v[0] == 1 && ((a+v[7])*(b+v[8])+c*a+(1-v[4])*(c*b))*v[6] < SHARED_PER_BLOCK) || (v[0] == 2 && v[5] == 1 && ((a+v[7])*(b+v[8])+(1-v[4])*(c*b))*v[6] < SHARED_PER_BLOCK) || (v[0] == 2 && ((a+v[7])*(b+v[8])+c*a+(1-v[4])*(c*b))*v[6] < SHARED_PER_BLOCK);};
-    tuner.addConstraint(kernelId, {"CACHING_STRATEGY", "SIZE_A", "SIZE_B", "SIZE_C", "DIRECT_WRITE", "GROUP_SIZE_Y", "GROUP_SIZE_Z", "PADD_AA", "PADD_AB"}, memConstraint);
-#define MAX_BLOCK_SIZE 1024
-    auto blockConstraint = [](const std::vector<size_t>&v) {return ((v[0]+v[2])*v[1]*v[3] < MAX_BLOCK_SIZE) && ((v[0]+v[2])*v[1]*v[3] >= 32);};
-    tuner.addConstraint(kernelId, {"SIZE_C", "GROUP_SIZE_Y", "PADD_C", "GROUP_SIZE_Z"}, blockConstraint);
-
-#if RAPID_TEST == 0
-    tuner.setReferenceClass(kernelId, std::make_unique<referenceGemm>(srcA, srcB, a, b, c, batch, dstId), std::vector<ktt::ArgumentId>{dstId});
-    tuner.setValidationMethod(ktt::ValidationMethod::SideBySideComparison, 0.001f);
-    tuner.setValidationRange(dstId, c*b*batch);
-#endif
-
-    tuner.setTuningManipulator(kernelId, std::make_unique<cTunableGemm>(batch, a, b, c));
-    
-    tuner.tuneKernel(kernelId);
-    tuner.printResult(kernelId, std::cout, ktt::PrintFormat::Verbose);
-    tuner.printResult(kernelId, "gemm_batch_output.csv", ktt::PrintFormat::CSV);
-
-    ktt::ComputationResult bestConf = tuner.getBestComputationResult(kernelId);
-
-    std::cout << "Performance: " << (double)(a*b*c*2)*(double)batch / (double)bestConf.getDuration() << " GFlops" << std::endl;
-    std::cout << "Memory BW: " << (double)(a*b+c*a+c*b)*(double)(batch)*(double)sizeof(REAL) / (double)bestConf.getDuration() << " GB/s" << std::endl;
-
-    return 0;
+int main(int argc, char **argv)
+{
+    auto gemmBatch = GemmBatch::Create<GemmBatch>(argc, argv, 4, "Examples/GemmBatch", "Gemm");
+    gemmBatch->Run();
 }
