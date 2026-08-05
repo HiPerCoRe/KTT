4f826c9ba7f3bfa038cd4345eda108b2c1bb984c
Petr Slonek <48959829+Petronous@users.noreply.github.com>
2026-08-05 14:39:20 +0200

    Implement CompilerTuningComponent
    Intended to provide a generic implementation for the comp. tuning functionality currently in CoulombSum2d.

--- Examples/CompilerTuningComponent.cpp
+++ Examples/CompilerTuningComponent.cpp
@@ -1,29 +1,52 @@
 #include "CompilerTuningComponent.h"
+#include "RunStats.hpp"
+
+using namespace std;
 
 CompilerTuningComponent::CompilerTuningComponent(ktt::Tuner &tuner, ktt::KernelId kernel)
-    : m_tuner(tuner), m_kernel(kernel) {
+    : m_tuner(tuner), m_kernel(kernel)
+{
     useSeparateTuning = false;
 }
 
-void CompilerTuningComponent::AddCompilerParameter(const std::string &name, const std::vector<std::string> &values) {
-    if (useSeparateTuning) {
-
-    }
+void CompilerTuningComponent::AddCompilerParameter(const string &name, const vector<string> &values)
+{
+    if (useSeparateTuning) m_tuner.AddSeparateCompilerParameter(m_kernel, name, values);
+    else m_tuner.AddCompilerParameter(m_kernel, name, values);
 }
 
-void CompilerTuningComponent::InitCLIOptions(std::vector<CliOption> &options) {
-    // TODO: Implement CLI options initialization
+void CompilerTuningComponent::InitCLIOptions(vector<CliOption> &options) {
+    options.emplace_back([this](const vector<string> &) {
+        useSeparateTuning = true;
+    }, "--sepCompTuning", "Enable separate compiler parameter tuning.");
 }
 
 void CompilerTuningComponent::Run() {
-    // TODO: Implement main execution logic
+    if (!useSeparateTuning) return;
+    auto bestConfig = m_tuner.GetBestConfiguration(m_kernel);
+    if (!bestConfig.IsValid()) {
+        cout << "No valid configuration was found. Skipping separate compiler parameter tuning.\n";
+        return;
+    }
+    std::cout << "\nTuning compiler options on top of best kernel configuration..." << std::endl;
+    const auto optResults = m_tuner.TuneOptions(m_kernel, bestConfig);
+    m_tuner.SaveResults(optResults, "CoulombSumOptionsOutput", ktt::OutputFormat::JSON);
+
+    RunStats stats;
+
+    for (const auto& optResult : optResults)
+    {
+        stats.Update(optResult);
+    }
+
+    stats.Print("Option tuning stats");
 }
 
 
-void NoCompilerTuning::AddCompilerParameter(const std::string &name, const std::vector<std::string> &values) {
+void NoCompilerTuning::AddCompilerParameter(const string &, const vector<string> &) {
 }
 
-void NoCompilerTuning::InitCLIOptions(std::vector<CliOption> &options) {
+void NoCompilerTuning::InitCLIOptions(vector<CliOption> &) {
 }
 
 void NoCompilerTuning::Run() {
