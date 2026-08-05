685b5adfd2b4a1d22fac655089c28e62d15ba0d5
Petr Slonek <48959829+Petronous@users.noreply.github.com>
2026-08-05 16:26:07 +0200

    Integrate CompilerTuningComponent with ExampleBase

--- Examples/CompilerTuningComponent.cpp
+++ Examples/CompilerTuningComponent.cpp
@@ -1,36 +1,38 @@
 #include "CompilerTuningComponent.h"
+#include "CliComponent.h"
 #include "RunStats.hpp"
+#include <cassert>
 
 using namespace std;
 
-CompilerTuningComponent::CompilerTuningComponent(ktt::Tuner &tuner, ktt::KernelId kernel)
-    : m_tuner(tuner), m_kernel(kernel)
-{
-    useSeparateTuning = false;
-}
+CompilerTuningComponent::CompilerTuningComponent(std::shared_ptr<ktt::Tuner> &tuner, ktt::KernelId &kernel):
+    m_tuner(tuner),
+    m_kernel(kernel),
+    useSeparateTuning(false)
+{}
 
 void CompilerTuningComponent::AddCompilerParameter(const string &name, const vector<string> &values)
 {
-    if (useSeparateTuning) m_tuner.AddSeparateCompilerParameter(m_kernel, name, values);
-    else m_tuner.AddCompilerParameter(m_kernel, name, values);
+    if (useSeparateTuning) m_tuner->AddSeparateCompilerParameter(m_kernel, name, values);
+    else m_tuner->AddCompilerParameter(m_kernel, name, values);
 }
 
-void CompilerTuningComponent::InitCLIOptions(vector<CliOption> &options) {
-    options.emplace_back([this](const vector<string> &) {
+void CompilerTuningComponent::InitCLIOptions(CliComponent &cli) {
+    cli.AddOption({[this](const vector<string> &) {
         useSeparateTuning = true;
-    }, "--sepCompTuning", "Enable separate compiler parameter tuning.");
+    }, "--sepCompTuning", "Enable separate compiler parameter tuning."});
 }
 
 void CompilerTuningComponent::Run() {
     if (!useSeparateTuning) return;
-    auto bestConfig = m_tuner.GetBestConfiguration(m_kernel);
+    auto bestConfig = m_tuner->GetBestConfiguration(m_kernel);
     if (!bestConfig.IsValid()) {
         cout << "No valid configuration was found. Skipping separate compiler parameter tuning.\n";
         return;
     }
     std::cout << "\nTuning compiler options on top of best kernel configuration..." << std::endl;
-    const auto optResults = m_tuner.TuneOptions(m_kernel, bestConfig);
-    m_tuner.SaveResults(optResults, "CoulombSumOptionsOutput", ktt::OutputFormat::JSON);
+    const auto optResults = m_tuner->TuneOptions(m_kernel, bestConfig);
+    m_tuner->SaveResults(optResults, "CoulombSumOptionsOutput", ktt::OutputFormat::JSON);
 
     RunStats stats;
 
@@ -42,11 +44,10 @@ void CompilerTuningComponent::Run() {
     stats.Print("Option tuning stats");
 }
 
-
 void NoCompilerTuning::AddCompilerParameter(const string &, const vector<string> &) {
 }
 
-void NoCompilerTuning::InitCLIOptions(vector<CliOption> &) {
+void NoCompilerTuning::InitCLIOptions(CliComponent &) {
 }
 
 void NoCompilerTuning::Run() {
--- Examples/CompilerTuningComponent.h
+++ Examples/CompilerTuningComponent.h
@@ -1,19 +1,20 @@
 #include "CliComponent.h"
-#include <Ktt.h>
+#include "Tuner.h"
+#include <memory>
 
 class CompilerTuningComponent {
 public:
-    CompilerTuningComponent(ktt::Tuner &tuner, ktt::KernelId kernel);
+    CompilerTuningComponent(std::shared_ptr<ktt::Tuner> &tuner, ktt::KernelId &kernel);
 
     // Public virtual despite NVI guidelines because they would just be thin wrappers otherwise.
     // Follows "Do not generalize prematurely" and "you aren't gonna need it"
-    virtual void InitCLIOptions(std::vector<CliOption> &options);
-    virtual void AddCompilerParameter(const std::string &name, const std::vector<std::string> &values);
+    virtual void InitCLIOptions(CliComponent &cli);
+    virtual void AddCompilerParameter(const std::string &name, const std::vector<std::string> &values = {});
     virtual void Run();
 
 protected:
-    ktt::Tuner &m_tuner;
-    ktt::KernelId m_kernel;
+    std::shared_ptr<ktt::Tuner> &m_tuner;
+    ktt::KernelId &m_kernel;
     bool useSeparateTuning;
 };
 
@@ -21,7 +22,7 @@ class NoCompilerTuning : public CompilerTuningComponent {
 public:
     using CompilerTuningComponent::CompilerTuningComponent;
 
-    void AddCompilerParameter(const std::string &name, const std::vector<std::string> &values) override;
-    void InitCLIOptions(std::vector<CliOption> &options) override;
+    void AddCompilerParameter(const std::string &name, const std::vector<std::string> &values = {}) override;
+    void InitCLIOptions(CliComponent &cli) override;
     void Run() override;
 };
