8f8f65f7de1fb8127330f890ea4c3882d2a52c6c
Petr Slonek <48959829+Petronous@users.noreply.github.com>
2026-08-10 10:27:45 +0200

    Cleanup: Remove commented-out code from CliComponent.h

--- Examples/CliComponent.h
+++ Examples/CliComponent.h
@@ -25,26 +25,6 @@ public:
     bool TryTrigger(int argc, char **argv, int &i) const;
 };
 
-// struct ExampleConfiguration
-// {
-//     bool rapidTest = false;
-//     bool useProfiling = false;
-//     unsigned platform = 0;
-//     unsigned device = 0;
-//     int problemSize = -1;
-//     std::string kernelFile = "";
-//     std::unique_ptr<ktt::StopCondition> stopCondition = nullptr;
-//     std::unique_ptr<ktt::Searcher> searcher = nullptr;
-//     std::string profileSearchModelPath = "";
-//     std::optional<ktt::PreciseMeasurementParameters> preciseParams = std::nullopt;
-//     bool useDynamicTuning = false;
-//     double dynamicTuningTime = 0;
-// };
-
-// struct ExampleRefKernelConfiguration : public ExampleConfiguration {
-//     std::string refKernelFile = "";
-// };
-
 class CliComponent {
 public:
     CliComponent();
